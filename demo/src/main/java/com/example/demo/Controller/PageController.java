367fbc7b5fb5af01442ae4e2d40569d5e94d173a
Casper <casper.aakviststeengaard@gmail.com>
2021-10-05 11:21:01 +0200

    Added Controller

---
+++ demo/src/main/java/com/example/demo/Controller/PageController.java
@@ -0,0 +1,11 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/")
+    public String index(){return "StartPage";}
+
+}
